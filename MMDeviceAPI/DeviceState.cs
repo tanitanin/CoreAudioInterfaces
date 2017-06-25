8fe29bce051d6771098e14b201329159eef9d1cd
中谷 勇太 <tajima2210@gmail.com>
2017-06-25 15:16:22 +0900

    MMDevice API の COM Interface を追加

---
+++ MMDeviceAPI/DeviceState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreAudio.MMDeviceAPI
+{
+    /// <summary>
+    /// Device State
+    /// </summary>
+    [Flags]
+    public enum DeviceState
+    {
+        /// <summary>
+        /// DEVICE_STATE_ACTIVE
+        /// </summary>
+        Active = 0x00000001,
+        /// <summary>
+        /// DEVICE_STATE_DISABLED
+        /// </summary>
+        Disabled = 0x00000002,
+        /// <summary>
+        /// DEVICE_STATE_NOTPRESENT 
+        /// </summary>
+        NotPresent = 0x00000004,
+        /// <summary>
+        /// DEVICE_STATE_UNPLUGGED
+        /// </summary>
+        Unplugged = 0x00000008,
+        /// <summary>
+        /// DEVICE_STATEMASK_ALL
+        /// </summary>
+        All = 0x0000000F
+    }
+}
